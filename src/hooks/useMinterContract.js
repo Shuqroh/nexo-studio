ce03834c8bb9e220031119c4d011653e06339ff5
Shuqroh <92368644+Shuqroh@users.noreply.github.com>
2022-07-05 23:55:06 +0100

    Delete src directory

--- src/hooks/useMinterContract.js
+++
@@ -1,7 +0,0 @@
-import { useContract } from "./useContract";
-import NexoStudioAbi from "../contracts/NexoStudio.json";
-import NexoStudioAddress from "../contracts/NexoStudio-address.json";
-
-// export interface for NFT contract
-export const useMinterContract = () =>
-  useContract(NexoStudioAbi.abi, NexoStudioAddress.NexoStudio);
