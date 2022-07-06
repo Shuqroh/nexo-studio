93562b93e87027f073d9979d0cdfd50988bfeb91
Shuqroh <92368644+Shuqroh@users.noreply.github.com>
2022-07-06 03:12:38 +0100

    Delete src directory

--- src/components/Loading.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Loading() {
-  return (
-    <div className="m-auto h-96 flex flex-col items-center justify-center overflow-y-hidden">
-      <h4 className="text-black">Loading, Please wait...</h4>
-    </div>
-  );
-}
--- src/components/TicketCard.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function TicketCard({ ticket }) {
-  return <div>TicketCard</div>;
-}
